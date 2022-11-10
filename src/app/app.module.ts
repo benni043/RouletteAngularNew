0960bab76537ca8f9585047a1dfe62a918877370
benni043 <benni.huff@gmail.com>
2022-11-10 08:26:09 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
